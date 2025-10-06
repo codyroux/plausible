6af3e593935fb42eee28eccc6ba92a3403613699
Cody Roux <codyroux@amazon.com>
2025-10-06 22:29:06 +0000

    fix: Remove references to Batteries.List to avoid clashes.

--- Plausible/Chamelean/Utils.lean
+++ Plausible/Chamelean/Utils.lean
@@ -1,8 +1,6 @@
 
 import Lean
 
-import Batteries.Data.List.Basic
-
 open Lean Meta LocalContext Std
 
 
@@ -205,10 +203,18 @@ def replicateM [Monad m] (n : Nat) (action : m α) : m (List α) :=
     let xs ← replicateM n action
     pure (x :: xs)
 
+
+def traverse {M : Type → Type} [Monad M] (f : α → M β) : List α → M (List β)
+  | [] => pure []
+  | x :: xs => do
+    let y ← f x
+    let ys ← traverse f xs
+    pure (y :: ys)
+
 /-- Converts a list of options to an optional list
     (akin to Haskell's `sequence`) -/
 def List.sequence (xs : List (Option α)) : Option (List α) :=
-  List.traverse id xs
+  traverse id xs
 
 /-- Helper function for splitting a list of triples into a triple of lists -/
 def splitThreeLists (abcs : List (α × β × γ)) : List α × List β × List γ :=
